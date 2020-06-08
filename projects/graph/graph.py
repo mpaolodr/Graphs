285784f5edf84658a0f4cdbbb8a50d373df07f92
mpaolodr <mpaolodr_1504@yahoo.com>
2020-06-08 15:27:29 -0400

    add_vertex, add_edge, and get neighbors done

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,19 +15,32 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # add v1 if it's not in self.vertices
+        # avoid errors
+        if v1 not in self.vertices:
+            self.vertices[v1] = set()
+
+        # add v2 if it's not in self.vertices
+        if v2 not in self.vertices:
+            self.vertices[v2] = set()
+
+        # add edge to v1
+        self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if vertex_id not in self.vertices:
+            return None
+
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
@@ -75,6 +90,7 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
