7e48a5c12393b22f4643c396b4d352d14c731605
mpaolodr <mpaolodr_1504@yahoo.com>
2020-06-12 10:37:18 -0400

    solution working but will certainly can be optimized

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -17,7 +17,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,6 +29,56 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# Barely passing solution! 1995 moves
+tg = {player.current_room.id: player.current_room.get_exits()}
+reverse = []
+opp_d = {"n": "s", "e": "w", "s": "n", "w": "e"}
+
+
+while len(tg) < len(world.rooms):
+
+    if player.current_room.id not in tg:
+        # add to traversal graph
+        tg[player.current_room.id] = player.current_room.get_exits()
+
+        # direction to travel
+        # remove direction on that vertex
+        direction = tg[player.current_room.id].pop()
+
+        # remove opposite direction on next vertex
+        player.travel(direction)
+        player.current_room.get_exits().remove(opp_d[direction])
+
+        # append to traversal path and reverse
+        traversal_path.append(direction)
+        reverse.append(opp_d[direction])
+
+    else:
+
+        if player.current_room.id in tg and len(tg[player.current_room.id]) > 0:
+            direction = tg[player.current_room.id].pop()
+
+            player.travel(direction)
+            player.current_room.get_exits().remove(opp_d[direction])
+
+            traversal_path.append(direction)
+            reverse.append(opp_d[direction])
+
+        else:
+            move_back = reverse.pop()
+
+            # node already visited
+            player.travel(move_back)
+            traversal_path.append(move_back)
+
+            if len(tg[player.current_room.id]) > 0:
+                direction = tg[player.current_room.id].pop()
+
+                player.travel(direction)
+                player.current_room.get_exits().remove(opp_d[direction])
+
+                traversal_path.append(direction)
+                reverse.append(opp_d[direction])
 
 
 # TRAVERSAL TEST
@@ -41,22 +91,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
